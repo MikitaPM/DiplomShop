f03d3517ae52c3bf065ddbfbf98b27a391947381
mikita <de4lt3774@gmail.com>
2023-05-21 16:15:43 +0300

    fix6

--- cart/cart.py
+++ cart/cart.py
@@ -4,60 +4,62 @@ from shop.models import Product
 
 
 class Cart(object):
-    """
-    Добавление продукта в корзину и изменение его кол-ва
-    """
 
     def __init__(self, request):
+        """
+        Инициализация корзины
+        """
         self.session = request.session
-
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
+            # сохраняем ПУСТУЮ корзину в сессии
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
     def __iter__(self):
         """
-        Перебор элементов из корзины и получение продуктов из базы
+        Перебираем товары в корзине и получаем товары из базы данных.
         """
         product_ids = self.cart.keys()
-        #Получение обьектов product и добавление их в корзину
+        # получаем товары и добавляем их в корзину
         products = Product.objects.filter(id__in=product_ids)
+
         cart = self.cart.copy()
         for product in products:
             cart[str(product.id)]['product'] = product
 
-        for item in self.cart.values():
+        for item in cart.values():
             item['price'] = Decimal(item['price'])
             item['total_price'] = item['price'] * item['quantity']
             yield item
-
+    
     def __len__(self):
         """
-        Подсчет всех товаров в корзине
+        Считаем сколько товаров в корзине
         """
         return sum(item['quantity'] for item in self.cart.values())
 
     def add(self, product, quantity=1, update_quantity=False):
+        """
+        Добавляем товар в корзину или обновляем его количество.
+        """
         product_id = str(product.id)
         if product_id not in self.cart:
-            self.cart[product_id] = {'quantity':0,
-                                     'price': str(product.price)}
+            self.cart[product_id] = {'quantity': 0,
+                                      'price': str(product.price)}
         if update_quantity:
             self.cart[product_id]['quantity'] = quantity
         else:
             self.cart[product_id]['quantity'] += quantity
-        self.save
+        self.save()
 
     def save(self):
-        # Обновление сессии
-        self.session[settings.CART_SESSION_ID] = self.cart
-        # Отметка сессии как изменённой
+        # сохраняем товар
         self.session.modified = True
 
     def remove(self, product):
         """
-        Удаление товаров из корзины
+        Удаляем товар
         """
         product_id = str(product.id)
         if product_id in self.cart:
@@ -65,14 +67,10 @@ class Cart(object):
             self.save()
 
     def get_total_price(self):
-        """
-        Общая сумма
-        """
-        return sum(Decimal(item['price'])* item['quantity'] for item in self.cart.values())
+        # получаем общую стоимость
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
 
     def clear(self):
-        """
-        Удаление корзины из сессии
-        """
+        # очищаем корзину в сессии
         del self.session[settings.CART_SESSION_ID]
         self.save()
--- cart/views.py
+++ cart/views.py
@@ -7,18 +7,16 @@ from .forms import CartAddProductForm
 
 @require_POST
 def cart_add(request, product_id):
-    """
-    Добавление продуктов в корзину
-    """
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
         cart.add(product=product,
-                quantity=cd['quantity'],
-                update_quantity=cd['update'])
+                 quantity=cd['quantity'],
+                 update_quantity=cd['update'])
     return redirect('cart:cart_detail')
+
 
 def cart_remove(request, product_id):
     cart = Cart(request)
@@ -26,10 +24,10 @@ def cart_remove(request, product_id):
     cart.remove(product)
     return redirect('cart:cart_detail')
 
+
 def cart_detail(request):
     cart = Cart(request)
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'],
-                                                                   'update':True})
-
+                                                                   'update': True})
     return render(request, 'cart/detail.html', {'cart': cart})
